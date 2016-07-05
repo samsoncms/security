dd11ad741bb72756eeae6a3301670687ebda7ca4
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-07-05 15:28:52 +0300

    Removed js traces

--- www/form/index.js
+++ www/form/index.js
@@ -3,7 +3,7 @@ s('#security .application-form').pageInit(function(parent) {
     // Bind select/un-select checkboxes
     s('.select-checkboxes-list-item input[type=checkbox]').ajaxClick(function(response, cb){
         cb.a('checked') ? cb.a('checked', false) : cb.a('checked', true);
-        s.trace(response);
-        s.trace(cb.a('checked'));
+        //s.trace(response);
+        //s.trace(cb.a('checked'));
     });
 });
